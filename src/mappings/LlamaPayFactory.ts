dd48e5fbd7f1ea4e7a7e335e384a064be4fdedde
nemusonaneko <nemusonaneko@protonmail.com>
2022-03-29 05:51:39 -0500

    Add Token entity and tracking

--- src/mappings/LlamaPayFactory.ts
+++ src/mappings/LlamaPayFactory.ts
@@ -2,9 +2,12 @@ import { Bytes } from "@graphprotocol/graph-ts";
 import { LlamaPayCreated } from "../../generated/templates/LlamaPay/LlamaPayFactory";
 import {
   LlamaPayContract,
-  LlamaPayFactory
+  LlamaPayFactory,
+  Token
 } from "../../generated/schema";
 import {LlamaPay} from "../../generated/templates"
+import {ERC20} from "../../generated/LlamaPayFactory/ERC20"
+
 
 // Address for Factory
 const factoryAddress = "0x068d6B8aD65679a741D7086c0cb96f8530B38494";
@@ -20,18 +23,26 @@ export function onLlamaPayCreated(event: LlamaPayCreated): void {
     factory.createdBlock = event.block.number;
   }
 
+  let erc20 = ERC20.bind(event.params.token);
+  let token = new Token(event.params.token.toHexString());
+  token.address = event.params.token;
+  token.symbol = (erc20.try_symbol()).value;
+  token.name = (erc20.try_name()).value;
+  token.decimals = (erc20.try_decimals()).value;
+
   // Create new contract node
   let contract = new LlamaPayContract(event.params.llamaPay.toHexString());
   
   contract.address = event.params.llamaPay;
   contract.factory = factory.id;
-  contract.token = event.params.token;
+  contract.token = token.id;
   contract.createdTimestamp = event.block.timestamp;
   contract.createdBlock = event.block.number;
 
   // Update mapping with new contract address
   LlamaPay.create(event.params.llamaPay);
 
+  token.save();
   contract.save();
   factory.count += 1;
   factory.save();
